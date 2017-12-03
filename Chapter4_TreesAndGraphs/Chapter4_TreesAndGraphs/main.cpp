6b59788a63cee6d3c7a551d01a9c7411694aa00d
JaeYoon Kim <jangikim@JaeYoons-MacBook-Pro.local>
2017-12-03 22:14:51 +1100

    Chapter4_Update_03122017

--- Chapter4_TreesAndGraphs/Chapter4_TreesAndGraphs/main.cpp
+++ Chapter4_TreesAndGraphs/Chapter4_TreesAndGraphs/main.cpp
@@ -8,15 +8,25 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <stack>s
+#include <list>
 using namespace std;
 
 //http://www.bogotobogo.com/cplusplus/binarytree.php
+
+enum State
+{
+    Unvisited, Visited, Visiting
+};
+
 typedef struct Tree
 {
     char data;
     Tree *left;
     Tree *right;
     Tree *parent;
+    State state;
 } TREE;
 
 TREE *newTreeNode(int data)
@@ -30,8 +40,10 @@ TREE *newTreeNode(int data)
     return node;
 }
 
-void insertTreeNode(TREE *node, char ch)
+//void insertTreeNode(TREE *node, char ch)
+struct Tree* insertTreeNode(TREE *node, int data)
 {
+    /*
     TREE *newNode = new TREE;
     newNode->data = ch;
     newNode->left = NULL;
@@ -56,7 +68,59 @@ void insertTreeNode(TREE *node, char ch)
                 node = node->right;
         }
     }
+    */
+    
+    static Tree *p;
+    Tree *retNode;
+    
+    //if(node != NULL) p = node;
+    
+    if(node == NULL)  {
+        retNode = newTreeNode(data);
+        retNode->parent = p;
+        return retNode;
+    }
+    if(data <= node->data ) {
+        p = node;
+        node->left = insertTreeNode(node->left,data);
+    }
+    else {
+        p = node;
+        node->right = insertTreeNode(node->right,data);
+    } 
+    return node;
+    
+    
 }
+
+void isBST(struct Tree *node)
+{
+    static int lastData = INT_MIN;
+    if(node == NULL) return;
+    
+    isBST(node->left);
+    
+    /* check if the given tree is BST */
+    if(lastData < node->data)
+        lastData = node->data;
+    else {
+        cout << "Not a BST" << endl;
+        return;
+    }
+    
+    isBST(node->right);
+    return;
+}
+
+int treeSize(struct Tree *node)
+{
+    if(node == NULL) return 0;
+    else
+        return treeSize(node->left) + 1 + treeSize(node->right);
+}
+
+
+
 
 TREE *getNode(TREE *node, char c)
 {
@@ -77,24 +141,112 @@ void printTreeInOrder(TREE *node)
     cout  << (int)node->data  << "-";
     printTreeInOrder(node->right);
 }
+
+/* recursion routine to find path */
+void pathFinder(struct Tree *node, int path[], int level, int mode)
+{
+    if(node == NULL) return;
+    // save leaf node
+    if(node->left == NULL && node->right == NULL) {
+        path[level] = node->data;
+        node->state = Unvisited;//jangikim
+        for(int i = 0; i <= level; i++) {
+            switch (mode) {
+                case 0:
+                    cout << (char)path[i];
+                    break;
+                case 1:
+                    cout << path[i];
+                default:
+                    break;
+            }
+        }
+        cout << endl;
+        return;
+    }
+    // save parent node
+    node->state = Unvisited;//jangikim
+    path[level] = node->data;
+    pathFinder(node->left, path, level+1, mode);
+    pathFinder(node->right, path, level+1, mode);
+}
+
+Tree* lookUp(struct Tree *node, char key)
+{
+    if(node == NULL) return node;
+    
+    if(node->data == key)
+        return node;
+    else {
+        if(node->data < key)
+            return lookUp(node->right, key) ;
+        else
+            return lookUp(node->left, key);
+    }
+}
+
 //4.1
 int maxDepth(Tree *root) {
-    if (root == nullptr ) {
+    //if(root == NULL)
+    if(root == NULL || (root->left == NULL && root->right == NULL))
         return 0;
-    }
     return 1 + max(maxDepth(root->left), maxDepth(root->right));
 }
 
 int minDepth(Tree *root) {
-    if (root == nullptr ) {
+    //if(root == NULL)
+    if(root == NULL || (root->left == NULL && root->right == NULL))
         return 0;
-    }
     return 1 + min(minDepth(root->left), minDepth(root->right));
 }
 
 bool isBalanced(Tree *root) {
     return (maxDepth(root) - minDepth(root) <= 1);
 }
+
+//4.2
+
+bool search(Tree *node, Tree *start, Tree *end)
+{
+    stack <Tree *> q;
+    Tree *u;
+
+    while(!q.empty())
+    {
+        q.top();
+    }
+    
+    q.push(start);
+    
+    while (!q.empty()) {
+        u = q.top();
+        q.pop();
+        if(u != NULL) {
+            if (u->state == Unvisited) {
+                if (u == end) {
+                    cout << u->data << endl;
+                    cout << "True in search return ..." << endl;
+                    return true;
+                } else {
+                    u->state = Visiting;
+                    if (u->left != NULL) {
+                        q.push(u->left);
+                    }
+                    if (u->right != NULL) {
+                        q.push(u->right);
+                    }
+                }
+            }
+            cout << u->data << endl;
+            u->state = Visited;
+        }
+    }
+    
+    cout << "False in search return ..." << endl;
+    
+    return false;
+}
+
 
 //4.3
 Tree * addToTree(int arr[], int start, int end){
@@ -113,11 +265,88 @@ Tree * createMinimalBST(int array[], int size) {
     return addToTree(array, 0, size-1);
 }
 
+//4.4
+/* get the level of a node element: root level = 0 */
+int getLevel(struct Tree *node, int elm, int level)
+{
+    if(node == NULL) return 0;
+    if(elm == node->data)
+        return level;
+    else if(elm < node->data)
+        return getLevel(node->left, elm, level+1);
+    else
+        return getLevel(node->right, elm, level+1);
+}
+
+/* This code prints out all nodes at the same depth (level) */
+void BreadthFirst_LevelElement_Print(struct Tree *root, vector<vector<int> > &v)
+{
+    if(root == NULL) return;
+    deque<Tree *> q;
+    q.push_back(root);
+    while(!q.empty()) {
+        Tree *p =  q.front();
+        int lev = getLevel(root, p->data, 0);
+        v[lev].push_back(p->data);
+        q.pop_front();
+        if(p->left) q.push_back(p->left);
+        if(p->right)q.push_back(p->right);
+    }
+    return;
+}
+
+/* levelPrint()
+ prints nodes at the same level
+ This is simpler than the BreadthFirstTraversal(root) above
+ It takes 2D vector with the same size of level (= MaxDepth+1)
+ and fills elements as we traverse (preOrder) */
+
+void levelPrint(struct Tree *node, vector<vector<char> >&elm, int level)
+{
+    if(node == NULL) return;
+    // leaf nodes
+    if(node->left == NULL && node->right == NULL) {
+        elm[level].push_back(node->data);
+        return;
+    }
+    // other nodes
+    elm[level++].push_back(node->data);
+    levelPrint(node->left, elm, level);
+    levelPrint(node->right, elm, level);
+}
+
+//4.5
+/* Tree Minimum */
+Tree* minTree(struct Tree *node)
+{
+    if(node == NULL) return NULL;
+    while(node->left)
+        node = node -> left;
+    return node;
+}
+
+/* In Order Successor - a node which has the next higher key */
+Tree *succesorInOrder(struct Tree *node)
+{
+    /* if the node has right child, seccessor is Tree-Minimum */
+    if(node->right != NULL) return minTree(node->right);
+    
+    Tree *y = node->parent;
+    while(y != NULL && node == y->right) {
+        node = y;
+        y = y->parent;
+    }
+    return y;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    char ch, ch1, ch2;
+    Tree *found;
+    Tree *succ;
+    
     std::cout << "Hello, World!\n";
     
-    //4.1
     TREE *root = newTreeNode('F');
     insertTreeNode(root,'B');
     insertTreeNode(root,'A');
@@ -128,12 +357,45 @@ int main(int argc, const char * argv[]) {
     insertTreeNode(root,'I');
     insertTreeNode(root,'H');
     
-    int maxDepthInt = maxDepth(root);
-    int minDepthInt = minDepth(root);
-    bool isBalancedInt = isBalanced(root);
+    /* is the tree BST? */
+    isBST(root);
     
+    /* size of tree */
+    cout << "size = " << treeSize(root) << endl;
     
-    //4.3
+    //4.1
+    /* max depth */
+    cout << "max depth = " << maxDepth(root) << endl;
+    
+    /* min depth */
+    cout << "min depth = " << minDepth(root) << endl;
+    
+    /* balanced tree? */
+    if(isBalanced(root))
+        cout << "This tree is balanced!\n";
+    else
+        cout << "This tree is not balanced!\n";
+    
+    /* printing all paths :
+     Given a binary tree, print out all of its root-to-leaf
+     paths, one per line. Uses a recursive helper to do the work. */
+    cout << "printing paths ..." << endl;
+    int path[10];
+    
+    //4.2
+    pathFinder(root, path, 0, 0);//Initialize state of tree into 'Unvisited'
+    //Test1 condition
+    //ch1 = 'F';
+    //ch2 = 'D';
+    //Test2 condition
+    ch1 = 'B';
+    ch2 = 'E';
+    //Test3 condition
+    //ch1 = 'E';
+    //ch2 = 'E';
+    search(root, lookUp(root,ch1), lookUp(root,ch2));
+    
+    //4.3 This is the same as the solution of bogotobogo.com.
     int array1[ 10 ] = {1,2,3,4,5,6,7,8,9,10};
     int sizeArray1 = sizeof(array1)/sizeof(*array1);
     Tree *root1 = createMinimalBST(array1, sizeArray1);
@@ -141,6 +403,76 @@ int main(int argc, const char * argv[]) {
     cout << "Tree start" << "\n";
     printTreeInOrder(root1);
     cout << "\n" << "Tree end" << "\n";
+    
+    /* max depth */
+    cout << "max depth = " << maxDepth(root1) << endl;
+    int path1[10];
+    pathFinder(root1, path1, 0, 1);//Initialize state of tree into 'Unvisited'
+    
+    //4.4 This is the same as the solution of bogotobogo.com.
+    /* Prints all element at the same depth (level) */
+    int row = maxDepth(root);
+    vector<vector<int> > levVec(row+1);
+    BreadthFirst_LevelElement_Print(root, levVec);
+    for(int m = 0; m < levVec.size(); m++) {
+        cout << "Level at " << m << ": ";
+        for(int n = 0; n < levVec[m].size(); n++)
+            cout << (char)levVec[m][n] << " ";
+        cout << endl;
+    }
+    
+    /* levelPrint()
+     prints nodes at the same level
+     This is simpler than the BreadthFirstTraversal(root) above
+     It takes 2D vector (elm) with the same size of level (= MaxDepth+1)
+     and fills elements as we traverse (preOrder) */
+    vector<vector<char> > elm;
+    int mxDepth = maxDepth(root);
+    elm.resize(mxDepth+1);
+    int level = 0;
+    levelPrint(root, elm, level);
+    cout << "levelPrint() " << endl;
+    for(int i = 0; i <= mxDepth; i++) {
+        cout << "level " << i << ": " ;
+        for(int j = 0; j < elm[i].size(); j++)
+            cout << elm[i][j] << " ";
+        cout << endl;
+    }
+    
+    //4.5 This is the same as the solution of bogotobogo.com.
+    ch = 'B';
+    found = lookUp(root,ch);
+    if(found) {
+        succ = succesorInOrder(found);
+        if(succ)
+            cout << "In Order Successor of " << ch << " is "
+            << succesorInOrder(found)->data << endl;
+        else
+            cout << "In Order Successor of " << ch << " is None\n";
+    }
+    
+    ch = 'E';
+    found = lookUp(root,ch);
+    if(found) {
+        succ = succesorInOrder(found);
+        if(succ)
+            cout << "In Order Successor of " << ch << " is "
+            << succesorInOrder(found)->data << endl;
+        else 
+            cout << "In Order Successor of " << ch << " is None\n";
+    }
+    
+    ch = 'I';
+    found = lookUp(root,ch);
+    if(found) {
+        succ = succesorInOrder(found);
+        if(succ)
+            cout << "In Order Successor of " << ch << " is "
+            << succesorInOrder(found)->data << endl;
+        else
+            cout << "In Order Successor of " << ch << " is None\n";
+    }
+    
     
     return 0;
 }
